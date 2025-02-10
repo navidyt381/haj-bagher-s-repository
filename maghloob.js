029008840ffce10ec04c9247f86ce3d6e98dea9c
Navid Nigga Lover <elhamnasyran575986@gmail.com>
2025-02-10 18:57:15 +0330

    Delete maghloob.js

--- maghloob.js
+++
@@ -1,9 +0,0 @@
-function maghloob() {
-    let number = document.getElementById('n1').value;
-    let reversedStr = "";
-    for (let i = number.length - 1; i >= 0; i--) {
-        reversedStr += number[i];
-    }
-    let reversedNumber = parseInt(reversedStr);
-    document.getElementById('result').innerText = "مغلوب عدد وارد شده: " + reversedNumber;
-}
