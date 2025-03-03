56f31b188478b9229d4e5e14c5657fee7853da76
Navid Nigga Lover <elhamnasyran575986@gmail.com>
2025-03-03 22:43:27 +0330

    Add files via upload

--- parsnovindb.sql
+++ parsnovindb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 17, 2025 at 06:36 PM
+-- Generation Time: Mar 03, 2025 at 07:09 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `newsly`
+--
+
+DROP TABLE IF EXISTS `newsly`;
+CREATE TABLE IF NOT EXISTS `newsly` (
+  `imageurl` varchar(1000) NOT NULL,
+  `title` varchar(1000) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `newsly`
+--
+
+INSERT INTO `newsly` (`imageurl`, `title`, `description`, `id`) VALUES
+('images/InShot_۲۰۲۴۰۱۱۷_۱۰۲۷۲۸۵۶۱ - Copy.png', 'گوز', 'عضله', 9),
+('images/InShot_۲۰۲۴۰۱۱۷_۱۰۲۶۱۹۷۶۹.png', 'دامتر استاپ', 'تو اتق منی؟', 8);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -31,17 +54,19 @@ DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `title` varchar(255) NOT NULL,
   `price` varchar(255) NOT NULL,
+  `image` varchar(3000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`title`, `price`, `id`) VALUES
-('معینی پور', '20000', 1),
-('مادر برد asus prime b760m-k', '8200000', 2);
+INSERT INTO `products` (`title`, `price`, `image`, `id`) VALUES
+('گوشی a12', '7500000', '', 3),
+('مادر برد asus prime b760m-k', '8200000', '', 2),
+('نان', '4200000', 'images/InShot_۲۰۲۴۰۱۱۷_۱۰۲۶۱۹۷۶۹.png', 4);
 
 -- --------------------------------------------------------
 
@@ -58,7 +83,14 @@ CREATE TABLE IF NOT EXISTS `table` (
   `repassword` varchar(48) NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `table`
+--
+
+INSERT INTO `table` (`name`, `family`, `email`, `password`, `repassword`, `id`) VALUES
+('نوید', 'طاهری', 'hajbagher@gmail.com', '1234', '1234', 4);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
